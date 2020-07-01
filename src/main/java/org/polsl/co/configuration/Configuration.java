c45ed2a3d292e19619fe4f310ba158ded2448215
GrzegorzC <gregcg@poczta.onet.pl>
2020-07-01 06:45:14 +0200

    Refactoring of various Lambda handlers

--- src/main/java/org/polsl/co/configuration/Configuration.java
+++ src/main/java/org/polsl/co/configuration/Configuration.java
@@ -4,6 +4,8 @@ public class Configuration {
 
     public static final String AIR_POLLUTION_TOPIC = "arn:aws:sns:us-east-2:464446151961:email-notification-topic";
 
+    public static final String AIR_QUALITY_THRESHOLDS_TABLE = "thresholds";
+
     private Configuration() {
     }
 }
